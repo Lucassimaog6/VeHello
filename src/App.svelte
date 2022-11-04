3fbc48b3af4edce415f87975a243c9bd329d4a60
Lucas Ferreira Simão <22101918@aluno.cotemig.com.br>
2022-11-04 18:55:23 -0300

    change website title and favicon

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script>
+  import Logo from "./img/logo.svg"
   import Header from "./components/Header.svelte"
   import Main from "./components/Main.svelte"
 </script>
+
+<svelte:head> // Local onde modificamos o "head" do site!
+  <title>Vehello</title>
+
+  // Favicon do site
+  <link rel="shortcut icon" href={Logo} type="image/x-icon">
+</svelte:head>
 
 <main>
   <Header />
